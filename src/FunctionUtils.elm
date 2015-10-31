7ffe083d990a687a73f033e1eee2ac8dfc11d809
Erik Peldan <epe@trifork.com>
2015-10-31 10:31:57 +0100

    Be more concise

--- src/FunctionUtils.elm
+++ src/FunctionUtils.elm
@@ -1,5 +1,10 @@
 module FunctionUtils where
 
+
 -- Ignores the first argument.
 -- Useful for converting a unary function into binary
-ignore1 f _ y = f y
+ignore1 f _ = f
+
+
+iterate : Int -> (x -> x) -> x -> x
+iterate n f = List.foldl (>>) identity (List.repeat n f)
