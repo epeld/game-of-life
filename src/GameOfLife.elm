4eedd2e23ed4a2c9513d4aaba2a23a6cff481009
Erik Peldan <epe@trifork.com>
2015-10-31 14:08:40 +0100

    Fix broken game of life rule logic

--- src/GameOfLife.elm
+++ src/GameOfLife.elm
@@ -1,20 +1,24 @@
 module GameOfLife where
-import Histogram exposing (Histogram, histogram, occurances)
+import Histogram exposing (Histogram, histogram, keysWithCount)
 import FunctionUtils exposing (iterate, ignore1)
 
 import Signal
 import List
 import List.Extra exposing (andThen)
 import Maybe
-import Set exposing (Set)
+import Set exposing (Set, diff, union, intersect)
 
 
 type alias Cell = (Int, Int)
 type alias Generation = Set Cell
 
 
-cells : List Cell -> Generation
-cells = Set.fromList
+fromCells : List Cell -> Generation
+fromCells = Set.fromList
+
+
+toCells : Generation -> List Cell
+toCells = Set.toList
 
 
 signal : Generation -> Signal a -> Signal Generation
@@ -25,27 +29,46 @@ generations : Int -> Generation -> Generation
 generations n g = iterate n generation g
 
 
-generation : Generation -> Generation
-generation xs = 
-    let 
-        remain = Set.filter (\x -> occurances x hist == 2) candidates
-        born = Set.filter (\x -> occurances x hist > 2) candidates
+{-
+From wikipedia:
 
-        candidates = Histogram.keys hist
-        hist = neighborHisto xs
+For each new generation,
+
+1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+2. Any live cell with two or three live neighbours lives on to the next generation.
+3. Any live cell with more than three live neighbours dies, as if by over-population.
+4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+-}
+generation : Generation -> Generation
+generation g = 
+    let 
+        histo = neighborHisto g
+
+        two = keysWithCount 2 histo
+        three = keysWithCount 3 histo
     in
-        Set.union born (Set.intersect remain xs)
+        -- Rule 2: living cells with count 2 live 
+        -- Rule 2 & 4: cells with count 3 live
+        -- Rule 1 & 4: all else die
+        (g `intersect` two) `union` three
 
 
 neighborHisto : Set Cell -> Histogram Cell
-neighborHisto cells = histogram <| List.concatMap neighbors <| Set.toList cells
+--neighborHisto cells = histogram <| List.concatMap neighbors <| Set.toList cells
+neighborHisto cells = 
+    Set.toList cells 
+    |> List.concatMap neighbors 
+    |> histogram
 
 
 neighbors : Cell -> List Cell
-neighbors (x, y) = let offsets = [-1, 0, 1]
+neighbors (x, y) = let
+                       offsets = [-1, 0, 1]
                        cells = offsets `andThen` \i ->
                                offsets `andThen` \j ->
                                [(x + i, y + j)]
 
-                   in List.filter (\a -> a /= (x,y)) cells
+                   in
+                      List.filter (\a -> a /= (x,y)) cells
 
--- src/Histogram.elm
+++ src/Histogram.elm
@@ -8,14 +8,24 @@ import List.Extra exposing (group, zip)
 
 type alias Histogram comparable = Dict.Dict comparable Int
 
+
+filter = Dict.filter
+
+
+keysWithCount : Int -> Histogram comparable -> Set comparable
+keysWithCount n histo = filter (\_ c -> c == n) histo |> keys 
+
+
 histogram : List comparable -> Histogram comparable
 histogram xs = 
     let groups = group (List.sort xs)
         assocs = zip (List.filterMap List.head groups) (List.map List.length groups)
      in Dict.fromList assocs
 
+
 keys : Histogram comparable -> Set comparable
 keys hst = Set.fromList <| Dict.keys hst
+
 
 occurances : comparable -> Histogram comparable -> Int
 occurances x hst = Maybe.withDefault 0 <| Dict.get x hst
